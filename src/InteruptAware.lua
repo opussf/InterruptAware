1b1dd58c67e8457455609cd9363c8ed4cc3fdde2
opussf <github@zz9-za.com>
2021-01-03 13:31:13 -0800

    debugging
    You're in pretty good shape for the shape you are in.

--- src/InteruptAware.lua
+++ src/InteruptAware.lua
@@ -9,8 +9,6 @@ COLOR_END = "|r";
 
 InteruptAware_log = {}
 
-InteruptAware_options = { ["debugLevel"] = 1 }
-
 function InteruptAware.Print( msg, showName )
 	-- print to the chat frame
 	-- set showName to false to suppress the addon name printing
@@ -21,12 +19,20 @@ function InteruptAware.Print( msg, showName )
 	DEFAULT_CHAT_FRAME:AddMessage( msg )
 end
 
+LOG_CRITICAL = 1
+LOG_ERROR = 2
+LOG_WARNING = 3
+LOG_INFO = 4
+InteruptAware_options = { ["debugLevel"] = LOG_INFO }
+
 function InteruptAware.LogMsg( msg, debugLevel, alsoPrint )
 	-- debugLevel  (Always - nil), (Critical - 1), (Error - 2), (Warning - 3), (Info - 4)
 	if( debugLevel == nil ) or
 			( ( debugLevel and InteruptAware_options.debugLevel ) and InteruptAware_options.debugLevel >= debugLevel ) then
 		table.insert( InteruptAware_log, { [time()] = (debugLevel and debugLevel..": " or "" )..msg } )
-		InteruptAware.Print( msg )
+		if( alsoPrint ) then
+			InteruptAware.Print( msg )
+		end
 	end
 end
 function InteruptAware.OnLoad()
@@ -34,25 +40,19 @@ function InteruptAware.OnLoad()
 	InteruptAwareFrame:RegisterEvent( "COMBAT_LOG_EVENT_UNFILTERED" )
 end
 function InteruptAware.ADDON_LOADED()
-end
-function InteruptAware.COMBAT_LOG_EVENT_UNFILTERED()
-end
---[[
-function StripDice.VARIABLES_LOADED()
-	StripDiceFrame:UnregisterEvent( "VARIABLES_LOADED" )
-	StripDice.LogMsg( "v"..STRIPDICE_MSG_VERSION.." loaded." )
+	InteruptAware:UnregisterEvent( "ADDON_LOADED" )
 	local expireTS = time() - 604800
 	local pruneCount = 0
 	local minPrune = time()
 	local maxPrune = 0
 	local doPrune = true
 	while( doPrune ) do
-		if( StripDice_log and StripDice_log[1] ~= nil ) then    -- has to exist, and have something at index 1
-			for ts, _ in pairs( StripDice_log[1] ) do           -- look in the pairs, since we don't know the key value
+		if( InteruptAware_log and InteruptAware_log[1] ~= nil ) then    -- has to exist, and have something at index 1
+			for ts, _ in pairs( InteruptAware_log[1] ) do           -- look in the pairs, since we don't know the key value
 				if( ts < expireTS ) then                        -- if this is too old, remove it
 					maxPrune = math.max( maxPrune, ts )
 					minPrune = math.min( minPrune, ts )
-					table.remove( StripDice_log, 1 )
+					table.remove( InteruptAware_log, 1 )
 					pruneCount = pruneCount + 1
 				else                                            -- all others will be too young to delete, stop
 					doPrune = false
@@ -63,315 +63,65 @@ function StripDice.VARIABLES_LOADED()
 		end
 	end
 	if( pruneCount > 0 ) then
-		StripDice.LogMsg( "Pruned "..pruneCount.." log entries, from "..
+		InteruptAware.LogMsg( "Pruned "..pruneCount.." log entries, from "..
 			date( "%c", minPrune ).." to "..date( "%c", maxPrune ).."." )  -- set to (info - 4)?
 	end
 end
-function StripDice.GROUP_ROSTER_UPDATE()
-	local NumGroupMembers = GetNumGroupMembers()
-	if( NumGroupMembers == 0 ) then  -- turn off listening
-		if( StripDice.gameActive ) then
-			StripDice.LogMsg( "Deactivating Dice game.", 4 )
-		end
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_SYSTEM" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_SAY" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_PARTY" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_PARTY_LEADER" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_RAID" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_RAID_LEADER" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_RAID_WARNING" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_INSTANCE_CHAT" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_INSTANCE_CHAT_LEADER" )
-		StripDiceFrame:UnregisterEvent( "CHAT_MSG_YELL" )
-		StripDice.StopGame()
-		StripDice.gameActive = nil
-	elseif( NumGroupMembers > 0 and not StripDice.gameActive ) then
-		StripDice.LogMsg( "Dice game is active with "..NumGroupMembers.." in the group.", 4 )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_SYSTEM" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_SAY" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_PARTY" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_PARTY_LEADER" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_RAID" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_RAID_LEADER" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_RAID_WARNING" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_INSTANCE_CHAT" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_INSTANCE_CHAT_LEADER" )
-		StripDiceFrame:RegisterEvent( "CHAT_MSG_YELL" )
-		StripDice.gameActive = true
+InteruptAware.reportEvents = {
+	["SPELL_AURA_BROKEN"]  = true,
+	["SPELL_AURA_BROKEN_SPELL"] = true,
+	["SPELL_AURA_REMOVED"] = true,
+}
+function InteruptAware.COMBAT_LOG_EVENT_UNFILTERED()
+	local _, t, _, sourceID, sourceName, sourceFlags, sourceRaidFlags,
+			destID, destName, destFlags, _, spellID, spName, _, ext1, ext2, ext3 = CombatLogGetCurrentEventInfo()
+	print( t )
+	InteruptAware.LogMsg( t..": "..sourceName.." -> "..destName, LOG_INFO, true )
+
+	if InteruptAware.reportEvents[t] then
+		InteruptAware.LogMsg( "Tracking "..t, LOG_INFO, true )
+		InteruptAware.LogMsg( "<<<"..spellID..">>> on "..destName.." removed by "..sourceName, LOG_INFO, true )
 	end
-end
-function StripDice.RemoveIconFromOtherSettings( iconValue, skipTableName )
-	-- take the iconValue to search for
-	-- skip the named table
-	local popTable = { ["lowIcon"] = true, ["highIcon"] = true }
-	for settingTable, struct in pairs( StripDice_options ) do
-		--StripDice.LogMsg( "Table: "..settingTable, true )
-		if( settingTable ~= skipTableName ) then
-			--StripDice.LogMsg( "Remove from table", true )
-			for i, val in pairs( struct ) do
-				--StripDice.LogMsg( "table: "..settingTable.." value: "..val.." =? "..iconValue.."  index: "..i, true )
-				if( val == iconValue ) then
-					--StripDice.LogMsg( "Matched value, remove it.", true )
-					if( popTable[settingTable] ) then
-						--StripDice.LogMsg( "Popping from table at pos: "..i, true )
-						table.remove( StripDice_options[settingTable], i )
-					else
-						--StripDice.LogMsg( "Setting index "..i.." to nil.", true )
-						StripDice_options[settingTable][i] = nil
-					end
+
+--[[
+
+
+
+		elseif hostileDest and playerSource and (t == "SPELL_AURA_REFRESH" or t == "SPELL_AURA_APPLIED") and spellID ~= 24131 and spellInList(spellID, SpellsInfo.cc) then
+				--print("new cc")
+if (t == "SPELL_AURA_BROKEN_SPELL" or t == "SPELL_AURA_BROKEN" or t == "SPELL_AURA_REMOVED") and spellID ~= 24131 and hostileDest and spellInList(spellID, SpellsInfo.cc) then
+if t == "SPELL_AURA_BROKEN" then
+				local chatMsg = "<<<"..spellID..">>> on " .. destName .. " removed by " .. sourceName
+				local playerMsg = GetSpellLink(spellID) .. "\124r\124h\124h on \124cffff0000" .. destName .."\124r\124h\124h removed by " .. unitColor(sourceID) .. sourceName
+				if nextMsgTime < time() then
+					print("\124Hplayer::CombatInfo:"..time()..":"..chatMsg.."\124h\124cff8888ff[Info]\124r\124h\124h: " .. playerMsg)
+					nextMsgTime = time() + 1
 				end
-			end
-		end
-	end
-end
-function StripDice.ReportSettings()
-	StripDice.LogMsg( "Show settings", 4 )  -- Info
-	local reportTables = {
-			{ ["t"] = "highIcon", ["str"] = "High" },
-			{ ["t"] = "lowIcon", ["str"] = "Low" }
-	}
-	local reportTable = {}
-	for _, struct in ipairs( reportTables ) do
-		local count = 0
-		local iconList = {}
-		for _, iconNum in ipairs( StripDice_options[struct.t] or {} ) do
-			for iconName, num in pairs( StripDice.raidIconValues ) do
-				if( num == iconNum ) then
-					table.insert( iconList, "{"..iconName.."}" )
+
+			elseif t == "SPELL_AURA_BROKEN_SPELL" then
+				local chatMsg = "<<<"..spellID..">>> on " .. destName .. " removed by " .. sourceName .."'s " .. ">>>"..ext1.."<<<"
+				local playerMsg = GetSpellLink(spellID) .. "\124r\124h\124h on \124cffff0000" .. destName .."\124r\124h\124h removed by " .. unitColor(sourceID) .. sourceName .. "'s " .. GetSpellLink(ext1)
+				if nextMsgTime < time() then
+					print("\124Hplayer::CombatInfo:"..time()..":"..chatMsg.."\124h\124cff8888ff[Info]\124r\124h\124h: " .. playerMsg)
+					nextMsgTime = time() + 1
 				end
-			end
-		end
-		if( #iconList > 0 ) then
-			table.insert( reportTable, string.format( "%s: %s", struct.str, table.concat( iconList, ", " ) ) )
-		end
-	end
-	local iconList = {}
-	for val, iconNum in pairs( StripDice_options.specificRollIcon or {} ) do
-		for iconName, num in pairs( StripDice.raidIconValues ) do
-			if( num == iconNum ) then
-				table.insert( iconList, val.."-{"..iconName.."}" )
-			end
-		end
-	end
-	if( #iconList > 0 ) then
-		table.insert( reportTable, string.format( "Specific: %s", table.concat( iconList, ", " ) ) )
-	end
 
-	if( #reportTable > 0 ) then
-		StripDice.LogMsg( table.concat( reportTable, ", " ) ) -- nil = always
-	end
-end
-StripDice.PLAYER_ENTERING_WORLD = StripDice.GROUP_ROSTER_UPDATE
-function StripDice.CHAT_MSG_SAY( ... )
-	_, msg, language, _, _, other = ...
-	msg = string.lower( msg )
-	if( string.find( msg, "settings" ) ) then -- report the settings
-		StripDice.ReportSettings()
-	elseif( string.find( msg, "set" ) ) then  -- set is the key word here
-		--print( msg )
-		local hl = string.match( msg, "(low)" ) or string.match( msg, "(high)" )
-		if( hl ) then
-			local variableName = hl.."Icon"
-			local index = 0
-			for testString in string.gmatch( msg, "%S+" ) do
-				--StripDice.LogMsg( "testString: "..testString, true )
-				for iconName, iconValue in pairs( StripDice.raidIconValues ) do
-					if( string.match( testString, iconName ) ) then
-						if( index == 0 ) then StripDice_options[variableName] = {}; end
-						index = index + 1
-						StripDice_options[variableName][index] = ( iconValue > 0 and iconValue or nil )
-						StripDice.RemoveIconFromOtherSettings( iconValue, variableName )
-					end
+			elseif expired then
+				local chatMsg = "<<<"..spellID.. ">>> on " .. destName .. " expired"
+				local playerMsg = GetSpellLink(spellID) .. "\"\124r\124h\124h on \124cffff0000" .. destName .."\124r\124h\124h expired"
+				if nextMsgTime < time() then
+					print("\124Hplayer::CombatInfo:"..time()..":"..chatMsg.."\124h\124cff8888ff[Info]\124r\124h\124h: " .. playerMsg)
+					nextMsgTime = time() + 1
 				end
-			end
-		else  -- until the above is refactored to include this setting
-			local value = tonumber( string.match( msg, "(%d+)" ) )
-			if( value ) then
-				--print( "value: "..value )
-				for testString in string.gmatch( msg, "%S+" ) do
-					for iconName, iconValue in pairs( StripDice.raidIconValues ) do
-						if( string.match( testString, iconName ) ) then
-							--StripDice.LogMsg( "Found "..iconName.." in msg", true )
-							if( not StripDice_options.specificRollIcon ) then
-								StripDice_options.specificRollIcon = {}
-							end
-							StripDice_options.specificRollIcon[value] = ( iconValue > 0 and iconValue or nil )
-							StripDice.RemoveIconFromOtherSettings( iconValue, "specificRollIcon" )
-						end
-					end
+
+			elseif t == "SPELL_AURA_REMOVED" then
+				local chatMsg = "<<<"..spellID.. ">>> on " .. destName .. " removed"
+				local playerMsg = GetSpellLink(spellID) .. "\"\124r\124h\124h on \124cffff0000" .. destName .."\124r\124h\124h removed"
+				if nextMsgTime < time() then
+					print("\124Hplayer::CombatInfo:"..time()..":"..chatMsg.."\124h\124cff8888ff[Info]\124r\124h\124h: " .. playerMsg)
+					nextMsgTime = time() + 1
 				end
-			end
-		end
-		StripDice.ReportSettings()
-	elseif( string.find( msg, "roll" ) ) then  -- roll starts a roll
-		--StripDice.LogMsg( "msg:"..msg, true )
-		StripDice.StopGame()
-		StripDice.currentGame = time()
-		StripDice_games[ StripDice.currentGame ] = {}
-		StripDice.LogMsg( "A roll has been started.  Game: "..StripDice.currentGame )
 
-		local pruneCount = 0
-		for gameTS in pairs( StripDice_games ) do
-			if( gameTS + 604800 < StripDice.currentGame ) then
-				StripDice_games[gameTS] = nil
-				pruneCount = pruneCount + 1
 			end
-		end
-		if( pruneCount > 0 ) then
-			StripDice.LogMsg( "Pruned "..pruneCount.." old games.", 4 )
-		end
-	elseif( StripDice.currentGame and StripDice.currentGame + 60 < time() ) then  -- game is started, and older than 1 minute
-		StripDice.LogMsg( "Game timed out" )
-		StripDice.StopGame()
-	end
-end
-StripDice.CHAT_MSG_PARTY = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_PARTY_LEADER = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_RAID = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_RAID_LEADER = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_RAID_WARNING = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_INSTANCE_CHAT = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_INSTANCE_CHAT_LEADER = StripDice.CHAT_MSG_SAY
-StripDice.CHAT_MSG_YELL = StripDice.CHAT_MSG_SAY
-
-function StripDice.CHAT_MSG_SYSTEM( ... )
-	_, roll = ...
-	--StripDice.Print( roll )
-	local found, _, who, roll, low, high = string.find( roll, "(.+) rolls (%d+) %((%d+)%-(%d+)%)")
-	if( found ) then
-		roll = tonumber( roll )
-		low = tonumber( low )
-		high = tonumber( high )
-		StripDice.LogMsg( who.." rolled a "..roll.." in the range of ("..low.." - "..high..")", 4 ) -- info
-		if( StripDice.currentGame and StripDice.currentGame + 60 >= time() ) then
-			if( StripDice_games[StripDice.currentGame][who] ) then
-				DoEmote( "No", who )
-				StripDice.LogMsg( who.." has already rolled." )
-			else
-				StripDice_games[StripDice.currentGame][who] = roll
-			end
-			-- build sorted rolls table
-			local rolls = {}
-			for _,rolled in pairs( StripDice_games[StripDice.currentGame] ) do
-				table.insert( rolls, rolled )
-			end
-			table.sort( rolls )
-
-			-- build min and max from rolls
-			StripDice.min = {}
-			StripDice.max = {}
-			local numRolls = #rolls
-			for i = 1, numRolls do
-				--print( i..": "..rolls[i] )
-				table.insert( StripDice.min, rolls[i] )
-				table.insert( StripDice.max, rolls[numRolls - (i-1)] )
-			end
-
-			local numHigh = #StripDice_options.highIcon
-			local numLow = #StripDice_options.lowIcon
-
-			--print( "I have "..#rolls.." rolls." )
-			--print( "I need "..numHigh.." high rolls, and "..numLow.." low rolls ("..( numHigh + numLow )..")" )
-
-			-- find how many of what I need.
-			while( #rolls < ( numHigh + numLow ) ) do
-				if( numHigh > numLow ) then
-					--print( "High - 1" )
-					numHigh = numHigh - 1
-				elseif( numLow > numHigh ) then
-					numLow = numLow - 1
-					--print( "Low - 1" )
-				else
-					--print( "Both - 1" )
-					numLow = numLow - 1
-					numHigh = numHigh - 1
-				end
-			end
-			-- reset to at least 1
-			if( numHigh == 0 and #StripDice_options.highIcon >= 1 ) then numHigh = 1; end
-			if( numLow  == 0 and #StripDice_options.lowIcon >= 1 ) then numLow  = 1; end
-
-			numHigh = math.min( numHigh, #StripDice_options.highIcon )
-			numLow = math.min( numLow, #StripDice_options.lowIcon )
-
-			StripDice.LogMsg( "I need "..numHigh.." high rolls, and "..numLow.." low rolls ("..( numHigh + numLow )..")", 4 )
-			StripDice.LogMsg( "high icon count: "..#StripDice_options.highIcon.."   low icon count: "..#StripDice_options.lowIcon, 4 )
-
-			-- find who has the top n rolls
-			--print( "Find Max" )
-			StripDice.maxWho = {}
-			local who = {}
-			for rollIndex = 1, numHigh do
-				rollValue = StripDice.max[rollIndex]
-				for name, roll in pairs( StripDice_games[StripDice.currentGame] ) do
-					if( roll == rollValue and who[name] == nil ) then
-						who[name] = roll
-						table.insert( StripDice.maxWho, name )
-					end
-				end
-				--print( "rollIndex: "..rollIndex.." rollValue: "..rollValue )
-			end
-
-			--print( "Find Min" )
-			StripDice.minWho = {}
-			who = {}
-			for rollIndex = 1, numLow do
-				rollValue = StripDice.min[rollIndex]
-				for name, roll in pairs( StripDice_games[StripDice.currentGame] ) do
-					if( roll == rollValue and who[name] == nil ) then
-						who[name] = roll
-						table.insert( StripDice.minWho, name )
-					end
-				end
-				--print( "rollIndex: "..rollIndex.." rollValue: "..rollValue )
-			end
-			for i,name in ipairs( StripDice.minWho ) do
-				if( not name or not StripDice_options.lowIcon[i] ) then
-					StripDice.LogMsg( "Icon: "..(StripDice_options.lowIcon[i] or "nil" ) )
-					StripDice.LogMsg( "Name: "..(name or "nil" ) )
-				end
-				StripDice.LogMsg( "Min: Put "..(StripDice_options.lowIcon[i] or "nil?").." on "..( name or "nil?"), 4 )
-				SetRaidTarget( name, ( StripDice_options.lowIcon[i] or 0 ) )
-			end
-			for i,name in ipairs( StripDice.maxWho ) do
-				StripDice.LogMsg( "Max: Put "..StripDice_options.highIcon[i].." on "..name, 4 )
-				SetRaidTarget( name, ( StripDice_options.highIcon[i] or 0 ) )
-			end
-			--print( "Find Specific" )
-			StripDice.specificWho = {}
-			for name, roll in pairs( StripDice_games[StripDice.currentGame] ) do
-				if( StripDice_options.specificRollIcon and StripDice_options.specificRollIcon[roll] ) then
-					StripDice.LogMsg( "Specific: Put "..StripDice_options.specificRollIcon[roll].." on "..name, 4 )
-					table.insert( StripDice.specificWho, name )
-					SetRaidTarget( name, ( StripDice_options.specificRollIcon[roll] or 0 ) )
-				end
-			end
-		end
-	end
-end
-function StripDice.StopGame()
-	-- @TODO  'fix this too'
-	StripDice.currentGame = nil
-	if( StripDice.minWho ) then  -- Remove the icons
-		for _,name in pairs( StripDice.minWho ) do
-			SetRaidTarget( name, 0 )
-		end
-	end
-	StripDice.min = nil
-	StripDice.minWho = nil
-	if( StripDice.maxWho ) then  -- Remove the icons
-		for _,name in pairs( StripDice.maxWho ) do
-			SetRaidTarget( name, 0 )
-		end
-	end
-	StripDice.max = nil
-	StripDice.maxWho = nil
-	-- specificWho
-	if( StripDice.specificWho ) then
-		for _,name in pairs( StripDice.specificWho ) do
-			SetRaidTarget( name, 0 )
-		end
-	end
-	StripDice.specificWho = nil
-end
 ]]
+end
--- test/test.lua
+++ test/test.lua
@@ -2,14 +2,29 @@
 
 require "wowTest"
 
-ParseTOC( "../src/StripDice.toc" )
+ParseTOC( "../src/InteruptAware.toc" )
 
 test.outFileName = "testOut.xml"
+
+InteruptAwareFrame = CreateFrame()
 
 -- addon setup
 function test.before()
 end
 function test.after()
+	for i, log in pairs( InteruptAware_log ) do
+		for ts, msg in pairs( log ) do
+			print( ts..": "..msg )
+		end
+	end
+end
+	-- timestamp,event,hideCaster,srcGUID,srcName,srcFlags,srcFlags2,
+	--		targetGUID,targetName,targetFlags,targetFlags2,spellId = CombatLogGetCurrentEventInfo()
+
+function test.test_COMBAT_LOG_EVENT_UNFILTERED_01()
+	CombatLogCurrentEventInfo = { 0, "SWING_DAMAGE", "unknown", "Player-3661-06DB0FE4", "testPlayer", 1297, 0,
+			"Creature-0-3131-0-13377-2544-00004ACCAE", "Southern Sand Crawler", 68136, 0x0, 0 }
+	InteruptAware.COMBAT_LOG_EVENT_UNFILTERED( )
 end
 
 test.run()
